2a5a104a52b55e212549ea76e0c070bc01315a9e
sriramaccumed <srirama_savaram@accumedit.com>
2021-02-11 16:32:58 +0530

    Dict in commons.py

---
+++ agentService/agent/commons.py
@@ -0,0 +1,21 @@
+#dictionary use for urls
+
+dict={
+
+    "index":"index/",
+    "loginResult":"agentLogin/",
+    "imageUpload":'upload/',
+    "agentDetails":'agentProfile/',
+    "createDepartment":'createDepartment/',
+    "showDepartments":'showAllDepartments/',
+    "searchBydepartment":'searchByDepartment/',
+    "createRequest":'createRequest/',
+    "showRequests":'showRequests/',
+    "register":'register/',
+    "showAgents":'showAgents/',
+    "searchAgent":'searchByAgent/',
+    "advSearch":'advSearchByAgent/',
+    "unlockRequest":'<requestedBy>/unlockRequest/',
+    "imageData":'<int:id>/imageData/',
+    "deptData":'<dept>/deptData/',
+}
--- agentService/agent/urls.py
+++ agentService/agent/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 
 from .import views, restviews
 from rest_framework import routers
-
+from .commons import *
 
 # rest web services
 from .restviews import AgentImageAPIView
@@ -15,29 +15,30 @@ router = routers.DefaultRouter()
 urlpatterns=[
     path('rest/', include(router.urls)),
     path('rest/getAgents/photo', AgentImageAPIView.as_view(),name="getagent-page"),
-    path('', views.handleShowAgent, name="agent-page"),
-    path('index/', views.index, name="index-page"),
+    path('', views.handleShowAgent, name="agentLoginHome"),
+    path(dict["index"], views.index, name="index"),
     path('search/', views.search, name="search"),
-    path('register/', views.handleAgent, name="register-page"),
-    path('showAgents/', views.showAgents, name="showAgents-page"),
-    path('searchByAgent/', views.searchByAgent, name="searchAgents-page"),
-    path('agentLogin/', views.agentLogin, name="login-page"),
+    path(dict["register"], views.handleAgent, name="register"),
+    path(dict["showAgents"], views.showAgents, name="showAgents"),
+    path(dict["searchAgent"], views.searchByAgent, name="searchAgent"),
+    path(dict["advSearch"], views.advSearchByAgent, name="advSearch"),
+    path(dict["loginResult"], views.agentLogin, name="loginResult"),
     path('agentLogout/', views.agentLogout, name="logout-page"),
-    path('upload/', views.imageUpload,name="upload-page"),
+    path(dict["imageUpload"], views.imageUpload,name="imageUpload"),
     #path('supervisorImageUpload/', views.supervisorImageUpload,name="superUserImageUpload-page"),
-    path('agentProfile/', views.agentDetails, name="agentDetails"),
+    path(dict["agentDetails"], views.agentDetails, name="agentDetails"),
     path('editProfile/', views.editProfile, name="edit-page"),
     path('changePassword/', views.changePassword, name="changePassword-page"),
-    path('createDepartment/', views.createDepartment, name="createDepartment-page"),
-    path('showAllDepartments/', views.showAllDepartments, name="createDepartment-page"),
-    path('createRequest/', views.createRequest, name="createRequest-page"),
-    path('showRequests/', views.showRequests, name="showRequests-page"),
+    path(dict["createDepartment"], views.createDepartment, name="createDepartment"),
+    path(dict["showDepartments"], views.showAllDepartments, name="showDepartments"),
+    path(dict["createRequest"], views.createRequest, name="createRequest"),
+    path(dict["showRequests"], views.showRequests, name="showRequests"),
     #path('supervisorDetails/', views.supervisorDetails, name="showSupervisor-page"),
-    path('advSearchByAgent/', views.advSearchByAgent, name="advSearchByAgent-page"),
-    path('<requestedBy>/unlockRequest/', views.unlockRequest, name="unlockRequest"),
-    path('<int:id>/imageData/', views.imageData, name="imageData"),
-    path('<dept>/deptData/', views.deptData, name="deptData"),
-    path('searchByDepartment/',views.searchByDepartment,name="searchBydepartment-page"),
+
+    path(dict["unlockRequest"], views.unlockRequest, name="unlockRequest"),
+    path(dict["imageData"], views.imageData, name="imageData"),
+    path(dict["deptData"], views.deptData, name="deptData"),
+    path(dict["searchBydepartment"],views.searchByDepartment,name="searchBydepartment"),
 
     #path('showAllDepartments/', views.showAllDepartments, name="showAllDepartments-page"),
     #path('searchByDepartment/', views.searchByDepartment, name="showAllDepartments-page"),
